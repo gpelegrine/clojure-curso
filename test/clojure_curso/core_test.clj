5e018eead9a7de6e5d59157f4bceaeccba50ea75
Gabriel Pelegrine <gp.pelegrine@gmail.com>
2024-10-01 21:01:57 -0300

    wip

--- test/clojure_curso/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure-curso.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-curso.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
